088f9da0020ae64ce77c23da13f73ffd8ad6ae3f
TheGreatCO <thegreatco@thegreatco.com>
2024-11-01 17:17:27 -0400

    more stable rtu parsing

--- servertu.go
+++ servertu.go
@@ -1,11 +1,127 @@
 package mbserver
 
 import (
+	"fmt"
 	"io"
 	"log"
 
 	"github.com/goburrow/serial"
 )
+
+type rtuPacket struct {
+	address            byte
+	functionCode       byte
+	data               []byte
+	crc                []byte
+	expectedDataLength int
+}
+
+func newRtuPacket(b byte) *rtuPacket {
+	return &rtuPacket{address: b, data: make([]byte, 0), crc: make([]byte, 0)}
+}
+
+func (p *rtuPacket) Data() []byte {
+	data := make([]byte, 1+1+len(p.data)+2)
+	data[0] = p.address
+	data[1] = p.functionCode
+	copy(data[2:], p.data)
+	copy(data[2+len(p.data):], p.crc)
+	return data
+}
+
+func (p *rtuPacket) appendReadRegistersData(b byte) bool {
+	// We're still collecting address information
+	if len(p.data) < 2 {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 2 && len(p.data) < 4 {
+		// We're collecting number of registers/coils to read
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 4 {
+		p.crc = append(p.crc, b)
+		if len(p.crc) == 2 {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		panic("should not get here")
+	}
+}
+
+func (p *rtuPacket) appendWriteSingleRegisterData(b byte) bool {
+	// We're still collecting address information
+	if len(p.data) < 2 {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 2 && len(p.data) < 4 {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 4 {
+		p.crc = append(p.crc, b)
+		if len(p.crc) == 2 {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	panic("should not get here")
+}
+
+func (p *rtuPacket) appendWriteMultipleRegistersData(b byte) bool {
+	// We're still collecting address information
+	if len(p.data) < 2 {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 2 && len(p.data) < 4 {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) == 4 {
+		p.data = append(p.data, b)
+		p.expectedDataLength = int(b)
+		return false
+	} else if len(p.data) > 4 && len(p.data) < 5+p.expectedDataLength {
+		p.data = append(p.data, b)
+		return false
+	} else if len(p.data) >= 5+p.expectedDataLength {
+		p.crc = append(p.crc, b)
+		if len(p.crc) == 2 {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	panic("should not get here")
+}
+
+func (p *rtuPacket) appendData(b byte) (bool, error) {
+	if p.functionCode == 0 {
+		p.functionCode = b
+		return false, nil
+	}
+	switch p.functionCode {
+	case 1:
+		fallthrough
+	case 2:
+		fallthrough
+	case 3:
+		fallthrough
+	case 4:
+		return p.appendReadRegistersData(b), nil
+	case 5:
+		fallthrough
+	case 6:
+		return p.appendWriteSingleRegisterData(b), nil
+	case 15:
+		fallthrough
+	case 16:
+		return p.appendWriteMultipleRegistersData(b), nil
+	}
+	return false, fmt.Errorf("unknown function code % x", p.functionCode)
+}
 
 // ListenRTU starts the Modbus server listening to a serial device.
 // For example:  err := s.ListenRTU(&serial.Config{Address: "/dev/ttyUSB0"})
@@ -26,42 +142,53 @@ func (s *Server) ListenRTU(serialConfig *serial.Config) (err error) {
 }
 
 func (s *Server) acceptSerialRequests(port serial.Port) {
-	SkipFrameError:
+	b := make([]byte, 1)
+	var packet *rtuPacket
 	for {
-		select {
-		case <-s.portsCloseChan:
-			return
-		default:
-		}
-
-		buffer := make([]byte, 512)
-
-		bytesRead, err := port.Read(buffer)
+		bytesRead, err := port.Read(b)
 		if err != nil {
-			if err != io.EOF {
-				log.Printf("serial read error %v\n", err)
+			if err == serial.ErrTimeout {
+				// log.Printf("serial read timeout, tossing packet: %v\n", err)
+				// i = 0
+				continue
 			}
+			if err == io.EOF {
+				log.Printf("serial read eof, exiting: %v\n", err)
+				return
+			}
+			log.Printf("serial read error %v\n", err)
 			return
 		}
-
-		if bytesRead != 0 {
-
-			// Set the length of the packet to the number of read bytes.
-			packet := buffer[:bytesRead]
-
-			frame, err := NewRTUFrame(packet)
-			if err != nil {
-				log.Printf("bad serial frame error %v\n", err)
-				//The next line prevents RTU server from exiting when it receives a bad frame. Simply discard the erroneous 
-				//frame and wait for next frame by jumping back to the beginning of the 'for' loop.
-				log.Printf("Keep the RTU server running!!\n")
-				continue SkipFrameError
-				//return
+		if bytesRead == 0 {
+			continue
+		}
+		if bytesRead == 1 {
+			if packet == nil {
+				packet = newRtuPacket(b[0])
+				if packet.address != 0x5b {
+					packet = nil
+					continue
+				}
+			} else {
+				res, err := packet.appendData(b[0])
+				if err != nil {
+					log.Printf("error appending data: %v\n", err)
+					packet = nil
+					continue
+				}
+				if res {
+					frame, err := NewRTUFrame(packet.Data())
+					if err != nil {
+						log.Printf("bad serial frame error %v\n", err)
+						packet = nil
+						continue
+					}
+					request := &Request{port, frame}
+					s.requestChan <- request
+					packet = nil
+					continue
+				}
 			}
-
-			request := &Request{port, frame}
-
-			s.requestChan <- request
 		}
 	}
 }
